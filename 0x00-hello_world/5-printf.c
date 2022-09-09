341099c1e726e67931d0f983d1c594969ab6de36
olatimi205 <olavick784@gmail.com>
2022-09-08 19:42:45 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string a stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n"
+);
+	return (0);
+}
