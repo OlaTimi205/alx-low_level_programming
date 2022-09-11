34c2cf893e61ac38df220521e38e493563f9b584
olatimi205 <olavick784@gmail.com>
2022-09-11 02:16:26 -0700

    processor

--- 0x00-hello_world/5-prinf.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-/**
- * main - print the string in the put function
- *
- * Description: using the main function
- * this program prints "Programming is like building a multiligual puzzl  ppppp puzzle
- * Returnvoid)
- 
