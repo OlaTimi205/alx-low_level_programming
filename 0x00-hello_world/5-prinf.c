eba659e3ff55b3b1aaabae3e4cb44553184c086b
olatimi205 <olavick784@gmail.com>
2022-09-11 01:47:38 -0700

    printf.c

---
+++ 0x00-hello_world/5-prinf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multiligual puzzl  ppppp puzzle
+ * Returnvoid)
+ 
